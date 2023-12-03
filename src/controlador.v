aec98c0ae38a9187b883bf09d862a92a3a14bccd
dariel18 <dariel.arian18@gmail.com>
2023-12-03 14:41:56 -0300

    Resolvendo erro ao simular

--- src/controlador.v
+++ src/controlador.v
@@ -13,3 +13,101 @@ and(aum_out, not_high_in, low_in);
 
 endmodule
 
+
+
+module controlador_timer (clk, intervalo_ligar, tempo_ligado, saida);
+
+	input wire clk;
+	input wire [4:0] intervalo_ligar;
+	input wire [4:0] tempo_ligado;
+	output reg saida;
+	
+	reg clk_div_1000, clk_div_1000_000, clk_em_s, clk_em_min, clk_em_horas;
+	reg [9:0] contador1;
+	reg [9:0] contador2;
+	reg [9:0] contador3;
+	reg [9:0] contador_seg;
+	reg [9:0] contador_min;
+	
+	reg [4:0] contador;
+	reg [4:0] tempo_contado;
+
+	initial begin
+		clk_div_1000 <= 0;
+		clk_div_1000_000 <= 0;
+		clk_em_s <= 0;
+		
+		contador1 <= 0;
+		contador2 <= 0;
+		contador3 <= 0;
+		contador_seg <= 0;
+		contador_min <= 0;
+	
+		saida <= 1'b0;
+		contador <= contador + 1'b1;
+	end
+	
+	always @(posedge clk) begin
+		if(contador1 != 1000) begin
+			contador1 <= contador1 + 1'b1;
+		end else begin
+			clk_div_1000 <= ~clk_div_1000;
+			contador1 <= 0;
+		end
+	end
+	
+	always @(posedge clk_div_1000) begin
+		if(contador2 != 1000) begin
+			contador2 <= contador2 + 1'b1;
+		end else begin
+			clk_div_1000_000 <= ~clk_div_1000_000;
+			contador2 <= 0;
+		end
+	end
+	
+	always @(posedge clk_div_1000_000) begin
+		if(contador3 != 1000) begin
+			contador3 <= contador3 + 1'b1;
+		end else begin
+			clk_em_s <= ~clk_em_s;
+			contador3 <= 0;
+		end
+	end
+	
+	always @(posedge clk_em_s) begin
+		if(contador_seg != 60) begin
+			contador_seg <= contador_seg + 1'b1;
+		end else begin
+			clk_em_min <= ~clk_em_min;
+			contador_seg <= 0;
+		end
+	end
+	
+	always @(posedge clk_em_min) begin
+		if(contador_min != 60) begin
+			contador_min <= contador_min + 1'b1;
+		end else begin
+			clk_em_horas <= ~clk_em_horas;
+			contador_min <= 0;
+		end
+	end
+		
+	//USE
+	//clk_em_s -> para contar segundos
+	//clk_em_min -> para contar minutos
+	//clk_em_horas -> para contar horas
+	always @(posedge clk) begin
+		
+		if (contador != intervalo_ligar - 1'b1) begin
+			contador <= contador + 1'b1;
+			saida <= 1'b0;
+		end else if (tempo_contado != tempo_ligado) begin
+			tempo_contado <= tempo_contado + 1'b1;
+			saida <= 1'b1;
+		end else begin
+			tempo_contado <= 4'b0;
+			contador <= 4'b0;
+			saida <= 1'b0;
+		end
+	end
+endmodule
--- src/controlador_tb.v
+++ src/controlador_tb.v
@@ -4,18 +4,28 @@ module controlador_tb;
 
 reg low_in_umid, high_in_umid;
 reg low_in_temp, high_in_temp;
-reg buff_luz, buff_irrig;
+
+reg [0:4] buff_luz_int, buff_luz_lig;
+reg [0:4] buff_irrig_int, buff_irrig_lig;
+
 reg clk;
 
 wire aum_umid, dim_umid;
 wire aum_temp, dim_temp;
 wire out_luz, out_irrig;
 
-//controlador umid_ctrl (low_in_umid, high_in_umid, dim_umid, aum_umid);
-//controlador temp_ctrl (low_in_temp, high_in_temp, dim_temp, aum_temp);
+controlador_timer luz_ctrl (clk, buff_luz_int, buff_luz_lig, out_luz);
+controlador_timer irrig_ctrl (clk, buff_irrig_int, buff_irrig_lig, out_irrig);
 
-controlador_timer luz_ctrl (clk, 5'b01010, 5'b00010, out_luz);
-controlador_timer irrig_ctrl (clk, 5'b10000, 5'b00010, out_irrig);
+controlador umid_ctrl (low_in_umid, high_in_umid, dim_umid, aum_umid);
+controlador temp_ctrl (low_in_temp, high_in_temp, dim_temp, aum_temp);
+
+initial begin 
+	buff_luz_int = 5'b01010;
+	buff_luz_lig = 5'b00010;
+	buff_irrig_int = 5'b10000;
+	buff_irrig_lig = 5'b00010;
+end
 
 initial begin
 	clk = 0;	
